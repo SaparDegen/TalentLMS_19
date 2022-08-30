ccd8a58c14bcc79cd2add5783c8214f7c8c78a9e
SaparDegen <saparbekdegenbaev@gmail.com>
2022-08-30 20:45:26 +0600

    1

--- src/Days.java
+++ src/Days.java
@@ -1,11 +1,11 @@
 public enum Days {
-    MONDAY("Мен дуйшомбу куну Java программалоо тилин окуйм"),
-    TUESDAY("Шейшембиде мен Java тили боюнча практикалык сабак алам"),
-    WEDNESDAY("Шаршемби куну техникалык англис тилин окуйм"),
-    THURSDAY("Бейшемби куну англис тилин уйроном"),
-    FRIDAY("Жума куну мен жумшак жондомдор боюнча сабакка катышам"),
-    SATURDAY("Ишемби куну мен Java боюнча уй тапшырмаларды аткарам"),
-    SUNDAY("Жекшемби куну уй-булом менен эс алабыз");
+    MONDAY("Men duishombu kunu Java programmaloo tilin okuim"),
+    TUESDAY("Sheishembi kunu men Java tili boyuncha praktikalyk sabak alam"),
+    WEDNESDAY("Sharshembi kunu technicalyk Anglis tilin okuym"),
+    THURSDAY("Beishembi kunu Anglis tilin uyronom"),
+    FRIDAY("Zhuma kunu men zhumshak zhondomdor boyuncha sabakka katysham"),
+    SATURDAY("Ishembi kunu men Java boyuncha uy tapshirmalardy atkaram"),
+    SUNDAY("Zhekshembi kunu uy-bulom menen es alabyz");
     String planner;
     private Days(String planner) {
         this.planner = planner;
